066f0fc194048d328adfff86a07503f8ff6a5036
duuujin <enwls5626@gmail.com>
2025-10-26 16:23:06 +0900

    좋아요버튼

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <h1>영화 정보</h1>
-  <div>
-    <h3 class="bg-yellow" :style="textRed">{{ title }}</h3>
-    <p>개봉 : {{ year }}</p>
-    <p>장르 : {{ category }}</p>
+  <div v-for="(movie, i) in data" :key="i">
+    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+    <p>개봉 : {{ movie.year }}</p>
+    <p>장르 : {{ movie.category }}</p>
+    <button @:click="increseLike(i)">좋아요</button> <span>{{ movie.like }}</span>
   </div>
+  
 </template>
 
 <script>
@@ -12,10 +14,32 @@
     name : 'App',
     data(){
       return {
-        title:"노량",
-        year: 2023,
-        category: '액션, 드라마',
-        textRed: "color : red"
+        data: [
+          {
+            title:"노량",
+            year: 2023,
+            category: '액션, 드라마',
+            textRed: "color : red",
+            like: 0,
+          },
+          {
+            title:"아쿠아맨과 로스트 킹덤",
+            year: 2023,
+            category: "액션, 판타지, 어드벤쳐",
+            like: 0,
+          },
+          {
+            title: "3월의 휴가",
+            year:2023,
+            category:"판타지, 드라마",
+            like: 0,
+          }
+        ]
+      }
+    },
+    methods:{
+      increseLike(i){
+        this.data[i].like += 1;
       }
     }
   }
